97ccc119a4ce531b7fdd076f36f04e31d35ebdca
harshjain1412 <77060662+harshjain1412@users.noreply.github.com>
2021-06-13 15:06:23 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,12 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int arr[], int);
+{int i;
+int large = arr[0];
+
+for(i=1;i<n;i++)
+if(arr[i]>large)
+large = arr[i];
+return large;
+
+}
